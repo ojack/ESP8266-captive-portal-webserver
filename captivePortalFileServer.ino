f575132f4c4dcfa83aad84b406f4e25016d49478
Olivia Jack <ojackson@gmail.com>
2018-02-11 16:23:23 -0500

    example FS captive portal

--- captivePortalFileServer.ino
+++ captivePortalFileServer.ino
@@ -1,17 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+
+#define DBG_OUTPUT_PORT Serial
 
 const byte DNS_PORT = 53;
 const char *ssid = "FREE AMAZING WIFI";
-IPAddress apIP(192, 168, 1, 1);
-DNSServer dnsServer;
-ESP8266WebServer webServer(80);
 
-String responseHTML = ""
-  "<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body>"
-  "<h1>Hello World!</h1><p>This is a captive portal example. All requests will "
-  "be redirected here.</p></body></html>";
+IPAddress apIP(192, 168, 1, 1);
+
+DNSServer dnsServer;
+
+ESP8266WebServer webServer(80);
 
 void setup() {
   WiFi.mode(WIFI_AP);
@@ -22,10 +24,20 @@ void setup() {
   // provided IP to all DNS request
   dnsServer.start(DNS_PORT, "*", apIP);
 
-  // replay to all requests with same HTML
+  //start debug port
+  DBG_OUTPUT_PORT.begin(115200);
+  DBG_OUTPUT_PORT.print("\n");
+  DBG_OUTPUT_PORT.setDebugOutput(true);
+  SPIFFS.begin();
+
+  //redirect all traffic to index.html
   webServer.onNotFound([]() {
-    webServer.send(200, "text/html", responseHTML);
+    if(!handleFileRead(webServer.uri())){
+      const char *metaRefreshStr = "<head><meta http-equiv=\"refresh\" content=\"0; url=http://192.168.1.1/index.html\" /></head><body><p>redirecting...</p></body>";
+      webServer.send(200, "text/html", metaRefreshStr);
+    }
   });
+  
   webServer.begin();
 }
 
@@ -33,3 +45,40 @@ void loop() {
   dnsServer.processNextRequest();
   webServer.handleClient();
 }
+
+
+
+String getContentType(String filename){
+  if(webServer.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+//Given a file path, look for it in the SPIFFS file storage. Returns true if found, returns false if not found.
+bool handleFileRead(String path){
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = webServer.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
